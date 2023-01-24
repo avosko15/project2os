6e29a06bae577717a934af95a3dd64cfffa16516
Anna <73409556+avosko15@users.noreply.github.com>
2023-01-24 15:34:45 -0800

    first step

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+int x = 12;
+float y = 3.14;
+char s[] = "Hello, world!";
+int i;
+
+printf("x is %d, y is %f\n",x,y);
+printf("%s\n",s);
+
+for (i = 0; i < 5; ++i) {
+		int value = 5;
+		value = value * i;
+		printf("%d x 5 = %d\n", i, value);
+    }
+return 0;
+}
